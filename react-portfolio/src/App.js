bf2036e21739f178a4b11c4a797f7a73fd1c7b56
tkcannon <taylorkcannon@gmail.com>
2022-03-18 14:39:17 -0600

    setup header and nav

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './App.css'
 
 function App() {
 
@@ -8,8 +9,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-        
-      <Footer />
+      <main>
+
+      </main>
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ react-portfolio/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Navigation';
+
+function Header() {
+
+  return (
+    <header>
+      <h1>Taylor Cannon</h1>
+      <Nav />
+    </header>
+  )
+}
+
+export default Header;
