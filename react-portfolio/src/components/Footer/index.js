23f6cad608c0d373d23b8d0ccc8dfecebec51490
tkcannon <taylorkcannon@gmail.com>
2022-03-18 14:43:05 -0600

    setupt footer

---
+++ react-portfolio/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>(placholder: github)</li>
+        <li>(placholder: linkedin)</li>
+        <li>(placholder: stackskills)</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer;
