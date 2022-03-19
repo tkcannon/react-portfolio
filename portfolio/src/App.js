b90d9ce6a02770e2ec261ad5084c369fd8c9dc3a
tkcannon <taylorkcannon@gmail.com>
2022-03-19 05:01:12 -0600

    file restructure

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './pages/About';
 import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <main>
         {displayedContent === 'about' && <About />}
         {displayedContent === 'projects' && <Projects />}
+        {displayedContent === 'contact' && <Contact />}
       </main>
       <Footer />
     </div>
