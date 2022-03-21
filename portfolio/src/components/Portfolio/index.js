14905e058bebf9440bf7b0d4e8b490e9ea81805f
tkcannon <taylorkcannon@gmail.com>
2022-03-20 21:14:30 -0600

    added styling for github repo link

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -5,42 +5,42 @@ function Portfolio() {
 
   const [projects] = useState([
     {
-      title: 'Fleet Keeper',
+      title: 'Fleet Keeper ',
       description: 'Full Stack Project',
       link: 'https://whispering-hollows-87266.herokuapp.com/login',
       image: 'fleet-keeper.jpg',
       repo: 'https://github.com/cthorne87/fleet-keeper/'
     },
     {
-      title: 'Event-Buddy',
+      title: 'Event-Buddy ',
       description: 'Front End Project',
       link: 'https://dcarteciano.github.io/travel-buddy/',
       image: 'event-buddy.jpg',
       repo: 'https://github.com/dcarteciano/travel-buddy'
     },
     {
-      title: 'Work Day Scheduler',
+      title: 'Work Day Scheduler ',
       description: 'Javascript Challenge',
       link: 'https://tkcannon.github.io/work-day-scheduler/',
       image: 'work-day-scheduler.gif',
       repo: 'https://github.com/tkcannon/work-day-scheduler'
     },
     {
-      title: 'The Tech Blog',
+      title: 'The Tech Blog ',
       description: 'Full Stack Challenge',
       link: 'https://secure-temple-36331.herokuapp.com/',
       image: 'the-tech-blog.png',
       repo: 'https://github.com/tkcannon/the-tech-blog'
     },
     {
-      title: 'Weather Dashboard',
+      title: 'Weather Dashboard ',
       description: 'Front End Challenge',
       link: 'https://tkcannon.github.io/weather-dashboard/',
       image: 'weather-dash.png',
       repo: 'https://github.com/tkcannon/weather-dashboard'
     },
     {
-      title: 'Run Buddy',
+      title: 'Run Buddy ',
       description: 'Html Module',
       link: 'https://tkcannon.github.io/run-buddy',
       image: 'run-buddy.png',
--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -7,12 +7,11 @@ function Project({ project }) {
       <a href={project.link} target="_blank" rel="noopener noreferrer">
         <div className="project">
           <div className="project-details">
-            <div className="project-title">
-              <h3>{project.title}</h3>
+            <h3>{project.title}
               <a href={project.repo} target="_blank" rel="noopener noreferrer">
                 <i className="github-link fa-brands fa-github"></i>
               </a>
-            </div>
+            </h3>
             <p>{project.description}</p>
           </div>
           <img src={require('../../assets/images/' + project.image)} alt={project.title} />
