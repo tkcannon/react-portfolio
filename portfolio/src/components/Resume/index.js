0bdbeee3bbbd413b7c17745233bfbdd9b1c6f406
tkcannon <taylorkcannon@gmail.com>
2022-03-20 19:35:40 -0600

    added list items to resume

--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -4,24 +4,29 @@ function Resume() {
   return (
     <section className="resume-section">
       <h2>Resume</h2>
+      <button><i class="fa-solid fa-download"></i></button>
       <div>
         <h3>Front End</h3>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JAVASCRIPT</li>
+          <li>JQUERY</li>
+          <li>Responsive Design</li>
+          <li>REACT</li>
+          <li>BOOTSTRAP</li>
         </ul>
       </div>
       <div>
         <h3>Back End</h3>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>APIs</li>
+          <li>NODE</li>
+          <li>EXPRESS</li>
+          <li>MYSQL</li>
+          <li>SEQUELIZE</li>
+          <li>MONGO</li>
+          <li>MONGOOSE</li>
         </ul>
       </div>
     </section>
