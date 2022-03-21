45d85f183c8f720c9846da91b94a149dcaafed4f
tkcannon <taylorkcannon@gmail.com>
2022-03-20 20:55:12 -0600

    added github repo links to projects

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -6,39 +6,45 @@ function Portfolio() {
   const [projects] = useState([
     {
       title: 'Fleet Keeper',
-      description: '',
-      link: 'tkcannon.github.io/',
-      image: 'fleet-keeper.jpg'
+      description: 'Full Stack Project',
+      link: 'https://whispering-hollows-87266.herokuapp.com/login',
+      image: 'fleet-keeper.jpg',
+      repo: 'https://github.com/cthorne87/fleet-keeper/'
     },
     {
       title: 'Event-Buddy',
-      description: '',
-      link: 'tkcannon.github.io/',
-      image: 'event-buddy.jpg'
+      description: 'Front End Project',
+      link: 'https://dcarteciano.github.io/travel-buddy/',
+      image: 'event-buddy.jpg',
+      repo: 'https://github.com/dcarteciano/travel-buddy'
     },
     {
       title: 'Work Day Scheduler',
-      description: '',
+      description: 'Javascript Challenge',
       link: 'https://tkcannon.github.io/work-day-scheduler/',
-      image: 'work-day-scheduler.gif'
+      image: 'work-day-scheduler.gif',
+      repo: 'https://github.com/tkcannon/work-day-scheduler'
     },
     {
       title: 'The Tech Blog',
-      description: '',
+      description: 'Full Stack Challenge',
       link: 'https://secure-temple-36331.herokuapp.com/',
-      image: 'the-tech-blog.png'
+      image: 'the-tech-blog.png',
+      repo: 'https://github.com/tkcannon/the-tech-blog'
     },
     {
       title: 'Weather Dashboard',
-      description: '',
+      description: 'Front End Challenge',
       link: 'https://tkcannon.github.io/weather-dashboard/',
-      image: 'weather-dash.png'
+      image: 'weather-dash.png',
+      repo: 'https://github.com/tkcannon/weather-dashboard'
     },
     {
       title: 'Run Buddy',
-      description: 'A Module',
+      description: 'Html Module',
       link: 'https://tkcannon.github.io/run-buddy',
-      image: 'run-buddy.png'
+      image: 'run-buddy.png',
+      repo: 'https://github.com/tkcannon/run-buddy'
     }
 
   ], []);
--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -4,10 +4,15 @@ function Project({ project }) {
 
   return (
     <>
-      <a href={project.link}>
+      <a href={project.link} target="_blank" rel="noopener noreferrer">
         <div className="project">
           <div className="project-details">
-            <h3>{project.title}</h3>
+            <div className="project-title">
+              <h3>{project.title}</h3>
+              <a href={project.repo} target="_blank" rel="noopener noreferrer">
+                <i className="github-link fa-brands fa-github"></i>
+              </a>
+            </div>
             <p>{project.description}</p>
           </div>
           <img src={require('../../assets/images/' + project.image)} alt={project.title} />
